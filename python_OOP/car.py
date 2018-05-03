40a58596d6c1fbbaf7a196e826d40e00f267c819
Sarah Grayer <slawler689@gmail.com>
2018-05-03 16:27:07 -0500

    adding car.py

---
+++ python_OOP/car.py
@@ -0,0 +1,26 @@
+class Car(object):
+    def __init__(self, price, speed, fuel, mileage):
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        if self.price > 10000:
+            self.tax = "15%"
+        else:
+            self.tax = "12%"
+        self.display_all()
+
+    def display_all(self):
+        print "Price: $" + str(self.price)
+        print "Speed: " + str(self.speed) + "mph"
+        print "Fuel: " + self.fuel
+        print "Mileage: " + str(self.mileage) + "mpg"
+        print "Tax: " + self.tax
+        print "*************************************"
+
+car1 = Car(2000, 35, "Full", 15)
+car2 = Car(2000, 5, "Not Full", 105)
+car3 = Car(2000, 15, "Kind of Full", 95)
+car4 = Car(2000, 25, "Full", 25)
+car5 = Car(2000, 45, "Empty", 15)
+car6 = Car(20000000, 35, "Empty", 15)
